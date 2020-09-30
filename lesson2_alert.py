9301194285f948329db6656eef245f629c13baa7
okolc <akakolchin@gmail.com>
2020-09-30 16:26:24 +0300

    alerts

--- lesson2_alert.py
+++ lesson2_alert.py
@@ -1,18 +1,20 @@
 from selenium import webdriver
-import os
 import math
 import time
-import test
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
 browser = webdriver.Chrome(executable_path='C:\chromedriver\chromedriver.exe')
-link = 'http://suninjuly.github.io/file_input.html'
+link = 'http://suninjuly.github.io/alert_accept.html'
 try:
     browser.get(link)
-    firstname = browser.find_element_by_xpath("//*[@name='firstname']").send_keys('FirstName')
-    lasttname = browser.find_element_by_xpath("//*[@name='lastname']").send_keys('LastName')
-    email = browser.find_element_by_xpath("//*[@name='email']").send_keys('Email')
-    download = browser.find_element_by_xpath("//*[@id='file']")
-    current_dir = os.getcwd()
-    download.send_keys(current_dir)
+    first_button = browser.find_element_by_css_selector(".btn.btn-primary").click()
+    confirm = browser.switch_to.alert
+    confirm.accept()
+    x_element = browser.find_element_by_xpath("//*[@id='input_value']")
+    x = int(x_element.text)
+    result = calc(x)
+    input = browser.find_element_by_xpath('//*[@id="answer"]')
+    input.send_keys(result)
     submit = browser.find_element_by_css_selector('.btn.btn-primary').submit()
 finally:
     # успеваем скопировать код за 30 секунд
